370cb4a8fa87247fd72e607d6123bed00d5a0d76
hynra <hynra@yahoo.com>
2019-04-19 17:35:40 +0700

    using axios

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const qs = require("querystring");
 const request = require('request-promise-native');
 const fs = require("fs");
 const promisePipe = require("promisepipe");
+const axios = require("axios");
 
 const jwplayer = require('jwplayer-node')
 ({api_key: key, api_secret: secret});
@@ -17,12 +18,12 @@ let options = {
 
 };
 
-getFile = (path) =>{
+getFile = (path) => {
     return new Promise((resolve, reject) => {
         fs.readFile(path, function (err, file) {
             if (err) {
                 reject(err)
-            }else {
+            } else {
                 resolve(file);
             }
         });
@@ -44,16 +45,31 @@ run = async () => {
             Signature: qs.escape(data.data.link.query.Signature)
         };
 
+        let filePath = __dirname + '/test2.mp4';
+        let file = await getFile(filePath);
+        let path = data.data.media.key;
+        let url = data.data.link.protocol + "://" + data.data.link.address + "/" + path;
+
+        let result = await axios.put(url, file, {
+            headers: {
+                'Content-Type': "video/mp4"
+            },
+            query: query
+        });
+
+        console.log(result);
+
         //let stream = await promisePipe(fs.createReadStream(__dirname + '/sample.mp4'));
         //let stream = fs.createReadStream(__dirname + '/sample.mp4', {encoding: 'binary'});
-        let filePath = __dirname + '/sample.mp4';
+        /*let filePath = __dirname + '/test2.mp4';
         let file = await getFile(filePath);
         let path = data.data.media.key;
         let url = data.data.link.protocol + "://" + data.data.link.address + "/" + path;
         let _options = {
             method: 'PUT',
             headers: {
-                'Content-Type': 'video/mp4'
+                'Content-Type': 'video/mp4',
+                'Content-Disposition': 'attachment; filename="test2.mp4"'
             },
             uri: url,
             qs: query,
@@ -61,7 +77,14 @@ run = async () => {
         };
 
         let resp = await request(_options);
-        console.log(resp);
+        console.log(resp); */
+
+        /*fs.createReadStream(filePath).pipe(request(_options)).then(body => {
+            console.log(body);
+        }).catch(err => {
+            console.log(err);
+        });*/
+
 
     } catch (e) {
         console.log(e);
